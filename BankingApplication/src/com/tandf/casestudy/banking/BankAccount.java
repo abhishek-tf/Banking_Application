95813f266ff1a2fb9885abc81fa5bd064645ffd8
abhishek-tf <Abhishek.RNair@informa.com>
2026-05-19 14:22:46 +0530

    Bank Account Module

--- BankingApplication/src/com/tandf/casestudy/banking/BankAccount.java
+++ BankingApplication/src/com/tandf/casestudy/banking/BankAccount.java
@@ -1,5 +1,89 @@
 package com.tandf.casestudy.banking;
 
-abstract class BankAccount {
+public abstract class BankAccount {
 
+    private String accountNumber;
+    private CustomerAccount customer;
+    private double balance;
+
+    public BankAccount(){
+
+    }
+
+    public BankAccount(String accountNumber, CustomerAccount customer, double initialDeposit) {
+        if (accountNumber == null) {
+            throw new NullPointerException("Account number cannot be null or blank");
+        }
+        if (customer == null) {
+            throw new NullPointerException("Customer cannot be null");
+        }
+        if (initialDeposit < 0) {
+            throw new InvalidAmountException("Initial deposit cannot be negative");
+        }
+        this.accountNumber = accountNumber;
+        this.customer = customer;
+        this.balance = initialDeposit;
+    }
+
+   
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public CustomerAccount getCustomer() {
+        return customer;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setCustomer(CustomerAccount customer) {
+        this.customer = customer;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            throw new InvalidAmountException("Deposit amount must be greater than zero");
+        }
+        balance += amount;
+        System.out.println("Deposited " + amount + "  New balance: " + balance);
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            throw new InvalidAmountException("Withdraw amount must be greater than zero");
+        }
+        if (amount > balance) {
+            throw new InsufficientBalanceException("Insufficient balance. Available: " + balance);
+        }
+        balance -= amount;
+        System.out.println("Withdrew " + amount + " | New balance: " + balance);
+    }
+
+    public void transfer(BankAccount target, double amount) {
+        if (target == null) {
+            throw new NullPointerException("Target account cannot be null");
+        }
+        if (this == target || this.accountNumber.equals(target.accountNumber)) {
+            throw new IllegalArgumentException("Cannot transfer to the same account");
+        }
+        if (amount <= 0) {
+            throw new InvalidAmountException("Transfer amount must be greater than zero");
+        }
+        this.withdraw(amount);
+        target.deposit(amount);
+        System.out.println("Transferred " + amount + " from " + this.accountNumber + " to " + target.accountNumber);
+    }
+
+    public abstract void displayDetails();
 }
---
+++ BankingApplication/src/com/tandf/casestudy/banking/CurrentAccount.java
@@ -0,0 +1,61 @@
+package com.tandf.casestudy.banking;
+
+public class CurrentAccount extends BankAccount {
+
+    private static final double DEFAULT_OVERDRAFT_LIMIT = 10000.0;
+
+    private double overdraftLimit;
+
+    public CurrentAccount() {
+        super();
+        this.overdraftLimit = DEFAULT_OVERDRAFT_LIMIT;
+    }
+
+    public CurrentAccount(String accountNumber, CustomerAccount customer, double initialDeposit) {
+        super(accountNumber, customer, initialDeposit);
+        this.overdraftLimit = DEFAULT_OVERDRAFT_LIMIT;
+    }
+
+    public CurrentAccount(String accountNumber, CustomerAccount customer,
+                          double initialDeposit, double overdraftLimit) {
+        super(accountNumber, customer, initialDeposit);
+        if (overdraftLimit < 0) {
+            throw new IllegalArgumentException("Overdraft limit cannot be negative");
+        }
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    public double getOverdraftLimit() {
+        return overdraftLimit;
+    }
+
+    public void setOverdraftLimit(double overdraftLimit) {
+        if (overdraftLimit < 0) {
+            throw new IllegalArgumentException("Overdraft limit cannot be negative");
+        }
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            throw new InvalidAmountException("Withdraw amount must be greater than zero");
+        }
+        double availableFunds = getBalance() + overdraftLimit;
+        if (amount > availableFunds) {
+            throw new InsufficientBalanceException(
+                "Withdrawal denied. Amount exceeds balance plus overdraft limit. Available: "
+                    + availableFunds);
+        }
+        setBalance(getBalance() - amount);
+        System.out.println("Withdrew " + amount + " | New balance: " + getBalance());
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Account Number  : " + getAccountNumber());
+        System.out.println("Customer        : " + (getCustomer() != null ? getCustomer().getName() : "N/A"));
+        System.out.println("Balance         : " + getBalance());
+        System.out.println("Overdraft Limit : " + overdraftLimit);
+    }
+}
