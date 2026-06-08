c71a56e4133de67cdd38c3428fb345c2b58b0942
abhishek-tf <Abhishek.RNair@informa.com>
2026-06-08 17:40:25 +0530

    DB connection file

---
+++ BankingApplication/src/com/tandf/casestudy/banking/DBConnection.java
@@ -0,0 +1,34 @@
+package com.tandf.casestudy.banking;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnection {
+    private static final Properties props = new Properties();
+    static {
+         
+    try (InputStream input =
+         DBConnection.class.getClassLoader()
+                           .getResourceAsStream("db.properties")) 
+                           {
+    props.load(input);
+} catch(IOException e){
+    e.printStackTrace();
+}     
+    }
+   
+    
+    private static final String url = props.getProperty("db.url");
+    private static final String username = props.getProperty("db.username");
+    private static final String password = props.getProperty("db.password");
+    public static Connection getConnection() throws SQLException{
+       return DriverManager.getConnection(
+                    url,
+                    username,
+                    password);
+    }
+    
+}
--- BankingApplication/src/com/tandf/casestudy/banking/LogIOException.java
+++ BankingApplication/src/com/tandf/casestudy/banking/LogIOException.java
@@ -2,11 +2,7 @@ package com.tandf.casestudy.banking;
 
 import java.io.IOException;
 
-/*
- * Custom CHECKED exception for the File Logging module.
- * Thrown by FileLogger when writing to the log file fails for any
- * IO reason other than the file being missing.
- */
+
 public class LogIOException extends IOException {
 
     public LogIOException(String message) {
